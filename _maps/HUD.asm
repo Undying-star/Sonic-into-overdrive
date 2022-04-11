b012723fbc88fc036166deb026cb537334bc31d1
Undying-star <Yanmaikol0000@gmail.com>
2022-04-11 16:12:06 -0400

    New title screen pallete and level select

--- _maps/HUD.asm
+++ _maps/HUD.asm
@@ -1,9 +1,9 @@
-Map_4cb6:
-Map_4cb6_0: 	dc.w Map_4cb6_8-Map_4cb6
-Map_4cb6_2: 	dc.w Map_4cb6_3B-Map_4cb6
-Map_4cb6_4: 	dc.w Map_4cb6_6E-Map_4cb6
-Map_4cb6_6: 	dc.w Map_4cb6_A1-Map_4cb6
-Map_4cb6_8: 	dc.b $A
+Map_ad0e:
+Map_ad0e_0: 	dc.w Map_ad0e_8-Map_ad0e
+Map_ad0e_2: 	dc.w Map_ad0e_3B-Map_ad0e
+Map_ad0e_4: 	dc.w Map_ad0e_6E-Map_ad0e
+Map_ad0e_6: 	dc.w Map_ad0e_A1-Map_ad0e
+Map_ad0e_8: 	dc.b $A
 	dc.b $80, $D, $80, $0, $0
 	dc.b $80, $D, $80, $18, $20
 	dc.b $80, $D, $80, $20, $40
@@ -14,7 +14,7 @@ Map_4cb6_8: 	dc.b $A
 	dc.b $A0, $9, $80, $30, $30
 	dc.b $40, $5, $81, $A, $0
 	dc.b $40, $D, $81, $E, $10
-Map_4cb6_3B: 	dc.b $A
+Map_ad0e_3B: 	dc.b $A
 	dc.b $80, $D, $80, $0, $0
 	dc.b $80, $D, $80, $18, $20
 	dc.b $80, $D, $80, $20, $40
@@ -25,7 +25,7 @@ Map_4cb6_3B: 	dc.b $A
 	dc.b $A0, $9, $80, $30, $30
 	dc.b $40, $5, $81, $A, $0
 	dc.b $40, $D, $81, $E, $10
-Map_4cb6_6E: 	dc.b $A
+Map_ad0e_6E: 	dc.b $A
 	dc.b $80, $D, $80, $0, $0
 	dc.b $80, $D, $80, $18, $20
 	dc.b $80, $D, $80, $20, $40
@@ -36,7 +36,7 @@ Map_4cb6_6E: 	dc.b $A
 	dc.b $A0, $9, $80, $30, $30
 	dc.b $40, $5, $81, $A, $0
 	dc.b $40, $D, $81, $E, $10
-Map_4cb6_A1: 	dc.b $A
+Map_ad0e_A1: 	dc.b $A
 	dc.b $80, $D, $80, $0, $0
 	dc.b $80, $D, $80, $18, $20
 	dc.b $80, $D, $80, $20, $40
