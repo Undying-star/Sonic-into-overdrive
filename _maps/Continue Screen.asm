8357d730e5705ae73ea42c4d96edb8fc791330c2
Undying-star <Yanmaikol0000@gmail.com>
2022-04-18 19:02:01 -0400

    Alot of new shit

--- _maps/Continue Screen.asm
+++ _maps/Continue Screen.asm
@@ -1,44 +1,48 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Continue screen
-; ---------------------------------------------------------------------------
-Map_ContScr_internal:
-		dc.w M_Cont_text-Map_ContScr_internal
-		dc.w M_Cont_Sonic1-Map_ContScr_internal
-		dc.w M_Cont_Sonic2-Map_ContScr_internal
-		dc.w M_Cont_Sonic3-Map_ContScr_internal
-		dc.w M_Cont_oval-Map_ContScr_internal
-		dc.w M_Cont_Mini1-Map_ContScr_internal
-		dc.w M_Cont_Mini1-Map_ContScr_internal
-		dc.w M_Cont_Mini2-Map_ContScr_internal
-M_Cont_text:	dc.b $B
-		dc.b $F8, 5, 0,	$88, $C4 ; "CONTINUE", stars and countdown
-		dc.b $F8, 5, 0,	$B2, $D4
-		dc.b $F8, 5, 0,	$AE, $E4
-		dc.b $F8, 5, 0,	$C2, $F4
-		dc.b $F8, 1, 0,	$A0, 4
-		dc.b $F8, 5, 0,	$AE, $C
-		dc.b $F8, 5, 0,	$C6, $1C
-		dc.b $F8, 5, 0,	$90, $2C
-		dc.b $38, 5, $20, $21, $E8
-		dc.b $38, 5, $20, $21, 8
-		dc.b $36, 5, 1,	$FC, $F8
-M_Cont_Sonic1:	dc.b 3
-		dc.b 4,	5, 0, $15, $FC	; Sonic	on floor
-		dc.b $F4, $A, 0, 6, $EC
-		dc.b $F4, 6, 0,	$F, 4
-M_Cont_Sonic2:	dc.b 3
-		dc.b 4,	5, 0, $19, $FC	; Sonic	on floor #2
-		dc.b $F4, $A, 0, 6, $EC
-		dc.b $F4, 6, 0,	$F, 4
-M_Cont_Sonic3:	dc.b 3
-		dc.b 4,	5, 0, $1D, $FC	; Sonic	on floor #3
-		dc.b $F4, $A, 0, 6, $EC
-		dc.b $F4, 6, 0,	$F, 4
-M_Cont_oval:	dc.b 2
-		dc.b $60, 9, $20, 0, $E8 ; circle on the floor
-		dc.b $60, 9, $28, 0, 0
-M_Cont_Mini1:	dc.b 1
-		dc.b 0,	6, 0, $12, 0	; mini Sonic
-M_Cont_Mini2:	dc.b 1
-		dc.b 0,	6, 0, $18, 0	; mini Sonic #2
-		even
+Map_235e:
+Map_235e_0: 	dc.w Map_235e_10-Map_235e
+Map_235e_2: 	dc.w Map_235e_48-Map_235e
+Map_235e_4: 	dc.w Map_235e_62-Map_235e
+Map_235e_6: 	dc.w Map_235e_6D-Map_235e
+Map_235e_8: 	dc.w Map_235e_91-Map_235e
+Map_235e_A: 	dc.w Map_235e_9C-Map_235e
+Map_235e_C: 	dc.w Map_235e_A2-Map_235e
+Map_235e_E: 	dc.w Map_235e_A8-Map_235e
+Map_235e_10: 	dc.b $B
+	dc.b $F8, $5, $0, $2B, $C4
+	dc.b $F8, $5, $0, $42, $D4
+	dc.b $F8, $5, $0, $3E, $E4
+	dc.b $F8, $5, $0, $52, $F4
+	dc.b $F8, $1, $0, $33, $4
+	dc.b $F8, $5, $0, $3E, $C
+	dc.b $F8, $5, $0, $56, $1C
+	dc.b $F8, $5, $0, $2F, $2C
+	dc.b $38, $5, $20, $12, $E8
+	dc.b $38, $5, $20, $12, $8
+	dc.b $36, $5, $1, $8C, $F8
+Map_235e_48: 	dc.b $5
+	dc.b $F6, $E, $0, $16, $E4
+	dc.b $E, $4, $0, $22, $EC
+	dc.b $EE, $0, $0, $24, $FC
+	dc.b $F6, $A, $0, $25, $4
+	dc.b $EE, $0, $0, $2E, $C
+Map_235e_62: 	dc.b $2
+	dc.b $F1, $F, $0, $35, $FC
+	dc.b $F9, $A, $0, $45, $E4
+Map_235e_6D: 	dc.b $7
+	dc.b $F6, $2, $0, $4E, $E3
+	dc.b $FE, $A, $0, $51, $EB
+	dc.b $F6, $C, $0, $5A, $F3
+	dc.b $EE, $0, $0, $5E, $FB
+	dc.b $FE, $9, $0, $5F, $3
+	dc.b $EE, $0, $0, $65, $C
+	dc.b $F6, $0, $0, $66, $14
+Map_235e_91: 	dc.b $2
+	dc.b $60, $9, $20, $0, $E8
+	dc.b $60, $9, $28, $0, $0
+Map_235e_9C: 	dc.b $1
+	dc.b $0, $6, $0, $6, $0
+Map_235e_A2: 	dc.b $1
+	dc.b $0, $6, $0, $6, $0
+Map_235e_A8: 	dc.b $1
+	dc.b $0, $6, $0, $C, $0
+	even
