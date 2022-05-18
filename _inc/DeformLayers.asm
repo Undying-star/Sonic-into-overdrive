e1bc93005c387873ab04e15daa70e6f9f31603c5
Undying-star <Yanmaikol0000@gmail.com>
2022-05-17 20:52:54 -0400

    its getting hot in green hill act 2

--- _inc/DeformLayers.asm
+++ _inc/DeformLayers.asm
@@ -48,53 +48,98 @@ Deform_Index:	dc.w Deform_GHZ-Deform_Index, Deform_LZ-Deform_Index
 ; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
 
 
-Deform_GHZ:       
+Deform_GHZ:
+        cmpi.b    #id_Title,(v_gamemode).w
+        beq.w    DeformLayers
+
+Deform_GHZ_Stage:
+        cmpi.b    #1,(v_act).w
+        beq.w    Deform_GHZ2
+
+Deform_GHZ1:
+	; block 3 - distant mountains
 		move.w	(v_scrshiftx).w,d4
 		ext.l	d4
 		asl.l	#5,d4
 		move.l	d4,d1
 		asl.l	#1,d4
 		add.l	d1,d4
-		moveq	#0,d5
-		bsr.w	ScrollBlock1
-		bsr.w	ScrollBlock4
+		moveq	#0,d6
+		bsr.w	BGScroll_Block3
+	; block 2 - hills & waterfalls
+		move.w	(v_scrshiftx).w,d4
+		ext.l	d4
+		asl.l	#7,d4
+		moveq	#0,d6
+		bsr.w	BGScroll_Block2
+	; calculate Y position
 		lea	(v_hscrolltablebuffer).w,a1
 		move.w	(v_screenposy).w,d0
 		andi.w	#$7FF,d0
 		lsr.w	#5,d0
 		neg.w	d0
-		addi.w	#$26,d0
-		move.w	d0,(v_bg2screenposy).w
+		addi.w	#$20,d0
+		bpl.s	@limitY
+		moveq	#0,d0
+	@limitY:
 		move.w	d0,d4
-		bsr.w	ScrollBlock3
-		move.w	(v_bgscreenposy).w,(v_bgscrposy_dup).w
-		move.w	#$6F,d1
-		sub.w	d4,d1
+		move.w	d0,(v_bgscrposy_dup).w
 		move.w	(v_screenposx).w,d0
 		cmpi.b	#id_Title,(v_gamemode).w
-		bne.s	loc_633C
-		moveq	#0,d0
-
-loc_633C:
+		bne.s	@notTitle
+		moveq	#0,d0	; reset foreground position in title screen
+	@notTitle:
 		neg.w	d0
 		swap	d0
-		move.w	(v_bgscreenposx).w,d0
+	; auto-scroll clouds
+		lea	(v_bgscroll_buffer).w,a2
+		addi.l	#$10000,(a2)+
+		addi.l	#$C000,(a2)+
+		addi.l	#$8000,(a2)+
+	; calculate background scroll	
+		move.w	(v_bgscroll_buffer).w,d0
+		add.w	(v_bg3screenposx).w,d0
 		neg.w	d0
-
-loc_6346:
+		move.w	#$1F,d1
+		sub.w	d4,d1
+		bcs.s	@gotoCloud2
+	@cloudLoop1:		; upper cloud (32px)
 		move.l	d0,(a1)+
-		dbf	d1,loc_6346
+		dbf	d1,@cloudLoop1
+
+	@gotoCloud2:
+		move.w	(v_bgscroll_buffer+4).w,d0
+		add.w	(v_bg3screenposx).w,d0
+		neg.w	d0
+		move.w	#$F,d1
+	@cloudLoop2:		; middle cloud (16px)
+		move.l	d0,(a1)+
+		dbf	d1,@cloudLoop2
+
+		move.w	(v_bgscroll_buffer+8).w,d0
+		add.w	(v_bg3screenposx).w,d0
+		neg.w	d0
+		move.w	#$F,d1
+	@cloudLoop3:		; lower cloud (16px)
+		move.l	d0,(a1)+
+		dbf	d1,@cloudLoop3
+
+		move.w	#$2F,d1
+		move.w	(v_bg3screenposx).w,d0
+		neg.w	d0
+	@mountainLoop:		; distant mountains (48px)
+		move.l	d0,(a1)+
+		dbf	d1,@mountainLoop
+
 		move.w	#$27,d1
 		move.w	(v_bg2screenposx).w,d0
 		neg.w	d0
-
-loc_6356:
+	@hillLoop:			; hills & waterfalls (40px)
 		move.l	d0,(a1)+
-		dbf	d1,loc_6356
+		dbf	d1,@hillLoop
+
 		move.w	(v_bg2screenposx).w,d0
-		addi.w	#0,d0
 		move.w	(v_screenposx).w,d2
-		addi.w	#-$200,d2
 		sub.w	d0,d2
 		ext.l	d2
 		asl.l	#8,d2
@@ -105,18 +150,139 @@ loc_6356:
 		move.w	d0,d3
 		move.w	#$47,d1
 		add.w	d4,d1
-
-loc_6384:
+	@waterLoop:			; water deformation
 		move.w	d3,d0
 		neg.w	d0
 		move.l	d0,(a1)+
 		swap	d3
 		add.l	d2,d3
 		swap	d3
-		dbf	d1,loc_6384
-		rts	
-; End of function Deform_GHZ
+		dbf	d1,@waterLoop
+		rts
 
+Deform_GHZ2:
+		move.w	(v_scrshiftx).w,d4
+		ext.l	d4
+		asl.l	#5,d4
+		move.l	d4,d1
+		asl.l	#1,d4
+		add.l	d1,d4
+		moveq	#0,d6
+		bsr.w	BGScroll_Block3
+	; block 2 - hills & waterfalls
+		move.w	(v_scrshiftx).w,d4
+		ext.l	d4
+		asl.l	#7,d4
+		moveq	#0,d6
+		bsr.w	BGScroll_Block2
+	; calculate Y position
+		lea	(v_hscrolltablebuffer).w,a1
+		move.w	(v_screenposy).w,d0
+		andi.w	#$7FF,d0
+		lsr.w	#5,d0
+		neg.w	d0
+		addi.w	#$20,d0
+		bpl.s	@limitY
+		moveq	#0,d0
+	@limitY:
+		move.w	d0,d4
+		move.w	d0,(v_bgscrposy_dup).w
+		move.w	(v_screenposx).w,d0
+		cmpi.b	#id_Title,(v_gamemode).w
+		bne.s	@notTitle
+		moveq	#0,d0	; reset foreground position in title screen
+	@notTitle:
+		neg.w	d0
+		swap	d0
+	; auto-scroll clouds
+		lea	(v_bgscroll_buffer).w,a2
+		addi.l	#$10000,(a2)+
+		addi.l	#$C000,(a2)+
+		addi.l	#$8000,(a2)+
+	; calculate background scroll
+		move.w	(v_bgscroll_buffer).w,d0
+		add.w	(v_bg3screenposx).w,d0
+		neg.w	d0
+		move.w	#$1F,d1
+		sub.w	d4,d1
+		bcs.s	@gotoCloud2
+	@cloudLoop1:		; upper cloud (32px)
+		move.l	d0,(a1)+
+		dbf	d1,@cloudLoop1
+
+	@gotoCloud2:
+		move.w	(v_bgscroll_buffer+4).w,d0
+		add.w	(v_bg3screenposx).w,d0
+		neg.w	d0
+		move.w	#$F,d1
+	@cloudLoop2:		; middle cloud (16px)
+		move.l	d0,(a1)+
+		dbf	d1,@cloudLoop2
+
+		move.w	(v_bgscroll_buffer+8).w,d0
+		add.w	(v_bg3screenposx).w,d0
+		neg.w	d0
+		move.w	#$F,d1
+	@cloudLoop3:		; lower cloud (16px)
+		move.l	d0,(a1)+
+		dbf	d1,@cloudLoop3
+
+		move.w	#$2F,d1
+		move.w	(v_bg3screenposx).w,d0
+		neg.w	d0
+	@mountainLoop:		; distant mountains (48px)
+		move.l	d0,(a1)+
+		dbf	d1,@mountainLoop
+
+		move.w	#$27,d1
+		move.w	(v_bg2screenposx).w,d0
+		neg.w	d0
+	@hillLoop:			; hills & waterfalls (40px)
+		move.l	d0,(a1)+
+		dbf	d1,@hillLoop
+
+		move.w	(v_bg2screenposx).w,d0
+		move.w	(v_screenposx).w,d2
+		sub.w	d0,d2
+		ext.l	d2
+		asl.l	#8,d2
+		divs.w	#$68,d2
+		ext.l	d2
+		asl.l	#8,d2
+		moveq	#0,d3
+		move.w	d0,d3
+		move.w	#$47,d1
+		add.w	d4,d1
+	;	move.w  d0,-(sp)
+	;	move.w  d1,-(sp)
+	;	move.w  d2,-(sp)
+	;	move.w  d3,-(sp)
+	;	move.w  d4,-(sp)
+	;	move.w  d5,-(sp)
+	;	move.w  d6,-(sp)
+	;	move.w  a1,-(sp)
+
+               ; move.w  (sp)+,a1
+              ;  move.w  (sp)+,d0
+             ;   move.w  (sp)+,d1
+            ;    move.w  (sp)+,d2
+           ;     move.w  (sp)+,d3
+          ;      move.w  (sp)+,d4
+         ;       move.w  (sp)+,d5
+        ;        move.w  (sp)+,d6
+
+	@waterLoop:			; water deformation
+		move.w	d3,d0
+		neg.w	d0
+		move.l	d0,(a1)+
+		swap	d3
+		add.l	d2,d3
+		swap	d3
+		dbf	d1,@waterLoop
+				bsr.s  LZeffectScroll
+
+		rts
+		;end of funny
 ; ---------------------------------------------------------------------------
 ; Labyrinth Zone background layer deformation code
 ; ---------------------------------------------------------------------------
